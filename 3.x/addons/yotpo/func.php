30cabec48e72d402823eb1b91908f9eeb8b49af6
Alon Braitstein <alon@yotpo.com>
2013-08-13 14:19:20 +0300

    1. Replace the select box of the widget language with a text input and link to codes

--- 3.x/addons/yotpo/func.php
+++ 3.x/addons/yotpo/func.php
@@ -271,4 +271,8 @@ function fn_yotpo_get_past_orders_button()
 	}
 
 }
+
+function fn_yotpo_select_language_link() {
+	return 'You can find the supported langauge codes <a href="http://support.yotpo.com/entries/21861473-Languages-Customization-" target="_blank">here</a></br>';	
+}
 ?>
